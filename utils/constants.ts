7dcc0d4cf21107f70fef76c12c8d5b5cce88d8f9
Goodie007 <aruoturegoodness@gmail.com>
2024-04-15 16:49:11 +0100

    set up

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,5 @@
-import { API_URL, CURRENT_ENV } from "@env";
+//import { API_URL, CURRENT_ENV } from "@env";
+//import { API_URL, CURRENT_ENV } from "@env";
 import Constants from "expo-constants";
 import * as Updates from "expo-updates";
 
@@ -6,15 +7,15 @@ import * as Updates from "expo-updates";
 
 const stillTestingArray = ["development", "testing"];
 
-export const currentEnvironment = CURRENT_ENV ?? "development";
+//export const currentEnvironment = CURRENT_ENV ?? "development";
 
-export const releaseChannel = Updates?.releaseChannel ?? currentEnvironment;
+//export const releaseChannel = Updates?.releaseChannel ?? currentEnvironment;
 
-export const productionOnly =
-  process.env.NODE_ENV === "production" &&
-  !stillTestingArray.includes(releaseChannel);
+// export const productionOnly =
+//   process.env.NODE_ENV === "production" &&
+//   !stillTestingArray.includes(releaseChannel);
 
-export const isDevelopmentRelease = Boolean(releaseChannel === "development");
+// export const isDevelopmentRelease = Boolean(releaseChannel === "development");
 
 export const isInExpoGo = Boolean(Constants.appOwnership === "expo");
 
@@ -24,8 +25,8 @@ export const app_storage = "@app-template.local.storage";
 export const app_theme_color_storage = "@app-template.theme.local.storage";
 
 // live url as default
-export const baseUrl =
-  process.env.API_URL ?? API_URL ?? "https://baseUrl";
+// export const baseUrl =
+//   process.env.API_URL ?? API_URL ?? "https://baseUrl";
 
 export const BACKGROUND_NOTIFICATION_TASK = "BACKGROUND-NOTIFICATION-TASK";
 
